ea992d54e7c5e87932ecaf81399979c057502b26
Claus <monkeydreamgo@gmail.com>
2022-05-27 20:57:52 +0800

    clean code && update github action

--- packages/mixstream-ui/config-overrides.js
+++ packages/mixstream-ui/config-overrides.js
@@ -1,17 +1,10 @@
-/*
- * @Author: yangliu
- * @Date: 2022-05-08 14:43:26
- * @LastEditTime: 2022-05-08 15:01:36
- * @LastEditors: yangliu
- */
-const { override, addWebpackExternals, addWebpackPlugin, addWebpackResolve } = require('customize-cra');
+const { override, addWebpackExternals, addWebpackPlugin } = require('customize-cra');
 const webpack = require('webpack');
 
 const env = process.env.NODE_ENV || 'development';
 
 const API_HOSTS = {
-  // development: 'http://localhost:3030', // disable this line for using remote server
-  development: 'http://mixstream-api.gz3.agoralab.co', // enable this line for using remote server
+  development: 'http://localhost:3030',
   production: 'https://mixstream-api.gz3.agoralab.co',
 };
 
